cf9500e18af28b336fc656f25f0fe3addfe80a06
Uttaraa <142016779+Uttaraa@users.noreply.github.com>
2024-02-05 22:04:07 +0530

    Add files via upload

---
+++ students/src/main/java/com/greatlearning/students/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.greatlearning.students.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greatlearning.students.model.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+}
